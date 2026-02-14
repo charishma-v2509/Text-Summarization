18f02162525cbd60bc7f3a88187990fb3eea2d2a
Sai Charishma Varkuti <charishmavarkuti09@gmail.com>
2026-02-14 13:25:49 +0530

    Removed client build serving for deployment

--- backend/app.js
+++ backend/app.js
@@ -12,12 +12,12 @@ app.use(express.json());
 app.use("/api", require("./routes/summaryRoutes"));
 
 // Serve React build in production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "..", "client", "build")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+//   });
+// }
 
 module.exports = app;
