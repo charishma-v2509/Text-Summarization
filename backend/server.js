2af9cd4fba7c63d11ad6481b5814aeadd5a104c8
Sai Charishma Varkuti <charishmavarkuti09@gmail.com>
2026-02-14 13:28:55 +0530

    Bind server to 0.0.0.0 for Render

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,6 @@
 const app = require("./app");
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0",  () => {
   console.log(`🚀 Server is running on http://localhost:${PORT}`);
 });
